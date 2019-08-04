1e9f1496e4ccb73b251993a293839f844d7554a8
lokonli <sander.lokerse@gmail.com>
2019-08-04 10:04:30 +0200

    Improve installation  script

--- README.md
+++ README.md
@@ -1,13 +1,23 @@
-# Dashticz 
+# Dashticz
 Alternative dashboard for Domoticz.
+
+The Dashboard of Domoticz is quite powerful. The disadvantage is that it's only possible to show information known in Domoticz.
+There is where Dashticz steps in. Dashticz is able to show (almost) all Domoticz information.
+In addition to that it's possible to show information from all kind of other sources.
 
 # Screenshots
 ![alt tag](http://i.imgur.com/9DBcpNd.jpg)
 
+# Installation instructions
+See https://dashticz.readthedocs.io/en/latest/gettingstarted/
+
+# Documentation and support
 Documentation can be found on:
 https://dashticz.readthedocs.io
 
 For additional information and support please visit the Dashticz forum:
 https://www.domoticz.com/forum/viewforum.php?f=67
+
+**Additional info**
 
 This currently is the active Dashticz repository. Previous repositories (dashticz/dashticz_v2 and dashticzv3/dashticz_v3) will not be updated anymore.
--- scripts/dashticz_install.sh
+++ scripts/dashticz_install.sh
@@ -5,20 +5,39 @@
 # . <(wget -qO - https://raw.githubusercontent.com/Dashticz/dashticz/beta/scripts/dashticz_install.sh )
 
 REPOSITORY="https://github.com/Dashticz/dashticz"
-myport=8082
+PORT=8082
+BRANCH=master
+SETTINGSFILE="./dashticz_install.ini"
+DEFAULTFOLDER="dashticz"
 
 echo This is the installation script for Dashticz
+echo
+
+if [[ -f "$SETTINGSFILE" ]]; then
+    echo "Using settings from $SETTINGSFILE"
+    . "$SETTINGSFILE" 
+else
+    echo "Using default settings"
+fi
+
+
+echo
+echo "Dashticz repository: $REPOSITORY"
+echo "Branch: $BRANCH"
+echo "Port: $PORT"
+echo
+
 retry=true
 
 while "$retry"; do
     echo The script will create a folder for Dashticz in the following location:
     echo `pwd`
     echo
-    read -p "Enter the folder name [dashticz]: " NAME
-    NAME=${NAME:-dashticz}
+    read -p "Enter the folder name [$DEFAULTFOLDER]: " NAME
+    NAME=${NAME:-$DEFAULTFOLDER}
     echo
     echo Dashticz will be installed in
-    echo ${NAME}
+    echo $NAME
     echo
     retry=false
     if [ -d "$NAME" ]; then
@@ -36,13 +55,13 @@ retry=true
 while "$retry"; do
     echo
     echo "Dashticz has a master branch (=stable) and a beta branch (=most up-to-date)"
-    read -p "Which branch to install? [master]: " BRANCH
-    BRANCH=${BRANCH:-master}
+    read -p "Which branch to install? [$BRANCH]: " MYBRANCH
+    BRANCH=${MYBRANCH:-$BRANCH}
     retry=false
-    if ! [[ "$BRANCH" =~ ^(master|beta)$ ]]; then
-        echo Please enter master or beta
-        retry=true
-    fi
+#    if ! [[ "$BRANCH" =~ ^(master|beta)$ ]]; then
+#        echo Please enter master or beta
+#        retry=true
+#    fi
 done
 echo
 echo "Now cloning the Dashticz repository $REPOSITORY"
@@ -50,6 +69,17 @@ echo
 git clone "$REPOSITORY" -b "$BRANCH" "$NAME" || ( echo Error. Something went wrong. Exiting; exit 1 )
 echo
 cd "$NAME"
+chmod a+rX .
+
+#Checking whether make is installed
+printf "Checking for make: "
+if [ $(dpkg-query -W -f='${Status}' make 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  echo "Ïnstalling ..."
+  sudo apt-get install make || ( echo Error. Something went wrong. Exiting; exit 1 ) ; 
+else
+    echo "[OK!]"
+fi
 
 echo "Configuring Dashticz."
 echo
@@ -65,25 +95,29 @@ echo "Finding a free port for Dashticz"
 retry=true
 while "$retry"; do
     # count the number of occurrences of port $myport in output: 1= in use; 0 = not in use
-    result=$(ss -ln src :$myport | grep -Ec -e "\<$myport\>")
+    result=$(ss -ln src :$PORT | grep -Ec -e "\<$PORT\>")
     if [ "$result" -eq 1 ]; then
-        echo "Port $myport is in use"
-        myport=$((myport + 1))
+        echo "Port $PORT is in use"
+        PORT=$((PORT + 1))
     else
-        echo "Port $myport is NOT in use"
+        echo "Port $PORT is available"
         retry=false
     fi
 done
 echo
-echo "Setting up Dashticz on port $myport"
+echo "Setting up Dashticz on port $PORT"
 echo
-#Checking whether make is installed
-if [ $(dpkg-query -W -f='${Status}' make 2>/dev/null | grep -c "ok installed") -eq 0 ];
-then
-  sudo apt-get install make;
-fi
+
+#creating Makefile.ini
+touch Makefile.ini
+
+echo "APP=dtv3-$PORT" >> Makefile.ini
+echo "PORT=$PORT" >> Makefile.ini
+
 echo
-make start PORT="$myport"
+echo "Starting make process ..."
+echo
+make start || ( echo "Error during Make. Exiting"; exit 1 )
 echo
 
 statusok=true
@@ -117,7 +151,11 @@ else
     echo "You might have to correct the Domoticz IP address or to configure the Domoticz username and/or password."
 fi
 
-echo All done
+echo "All done."
 echo
+echo "Visit the following link for information how to configure your Dashticz dashboard:"
+echo "https://dashticz.readthedocs.io/en/latest/gettingstarted/basicdashboard.html#step-2-creating-a-custom-layout"
+echo
+
 
 
