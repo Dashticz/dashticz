1329af8c291f9a34e2512067b4a1cecec138f1f1
lokonli <sander.lokerse@gmail.com>
2019-08-30 23:04:56 +0200

    [enh] async loading

--- js/version.js
+++ js/version.js
@@ -14,50 +14,54 @@ var python = '';
 var levelNamesEncoded = false;
 var levelNamesEncodeVersion = '3.9476'; /* Domoticz version above this, level names are encoded */
 
-    $.ajax({
+function initVersion() {
+
+    return $.ajax({
         url: 'version.txt',
-        async: false,
         dataType: 'json',
         cache:false,
         success: function (localdata) {
             dashticz_version = localdata.version;
             dashticz_branch = localdata.branch
         }
-    });
-if (typeof(config) === 'undefined'
-    || (typeof(config) !== 'undefined' && (typeof(config['disable_update_check']) === 'undefined' || !config['disable_update_check']))
-) {
-    $.ajax({
-        url: 'https://raw.githubusercontent.com/Dashticz/dashticz/' + dashticz_branch + '/version.txt',
-        async: false,
-        dataType: 'json',
-        success: function (data) {
-            var message = 'Latest changes made: ' + data.last_changes;
+    })
+    .then(function() {
+        if (typeof(config) === 'undefined'
+            || (typeof(config) !== 'undefined' && (typeof(config['disable_update_check']) === 'undefined' || !config['disable_update_check']))
+        ) {
+            return $.ajax({
+                url: 'https://raw.githubusercontent.com/Dashticz/dashticz/' + dashticz_branch + '/version.txt',
+                dataType: 'json',
+                success: function (data) {
+                    var message = 'Latest changes made: ' + data.last_changes;
 
-            if (dashticz_version !== data.version) {
-                moved = true;
-                newVersion = '<br><i>Version ' + data.version + ' is available! <a href="https://github.com/Dashticz/dashticz/tree/' + dashticz_branch + '" target="_blank">Click here to download</a></i><br><i>' + message + '</i>';
-            }
-            else if (dashticz_version === data.version) {
-                moved = false;
-                newVersion = '<br><i>You are running latest version.</i>';
-            }
-            if (moved == true) {
-                infoMessage(language.misc.new_version + '! (V' + data.version + ')', '<a href="https://github.com/Dashticz/dashticz/tree/' + dashticz_branch + '" target="_blank">' + language.misc.download + '</a>');
-            }
+                    if (dashticz_version !== data.version) {
+                        moved = true;
+                        newVersion = '<br><i>Version ' + data.version + ' is available! <a href="https://github.com/Dashticz/dashticz/tree/' + dashticz_branch + '" target="_blank">Click here to download</a></i><br><i>' + message + '</i>';
+                    }
+                    else if (dashticz_version === data.version) {
+                        moved = false;
+                        newVersion = '<br><i>You are running latest version.</i>';
+                    }
+                    if (moved == true) {
+                        infoMessage(language.misc.new_version + '! (V' + data.version + ')', '<a href="https://github.com/Dashticz/dashticz/tree/' + dashticz_branch + '" target="_blank">' + language.misc.download + '</a>');
+                    }
+                }
+            });
         }
+    })
+    .then(function() {
+        if (typeof(window.btoa(config['user_name'])) !== 'undefined' && window.btoa(config['pass_word']) !== '') loginCredentials = 'username=' + window.btoa(config['user_name']) + '&password=' + window.btoa(config['pass_word']) + '&';
+
+        return $.ajax({
+            url: config['domoticz_ip'] + '/json.htm?' + loginCredentials + 'type=command&param=getversion',
+            dataType: 'json',
+            success: function (data) {
+                domoversion = 'Domoticz version: ' + data.version;
+                dzVents = '<br>dzVents version: ' + data.dzvents_version;
+                python = '<br> Python version: ' + data.python_version;
+                levelNamesEncoded = (Number(data.version) >= Number(levelNamesEncodeVersion));
+            }
+        });
     });
 }
-    if (typeof(window.btoa(config['user_name'])) !== 'undefined' && window.btoa(config['pass_word']) !== '') loginCredentials = 'username=' + window.btoa(config['user_name']) + '&password=' + window.btoa(config['pass_word']) + '&';
-
-    $.ajax({
-        url: config['domoticz_ip'] + '/json.htm?' + loginCredentials + 'type=command&param=getversion',
-        async: false,
-        dataType: 'json',
-        success: function (data) {
-            domoversion = 'Domoticz version: ' + data.version;
-            dzVents = '<br>dzVents version: ' + data.dzvents_version;
-            python = '<br> Python version: ' + data.python_version;
-            levelNamesEncoded = (Number(data.version) >= Number(levelNamesEncodeVersion));
-        }
-    });
