724bee70c2fa1808c0940d3c0ca7d49b1f18702b
lokonli <sander.lokerse@example.com>
2020-02-14 00:39:07 +0100

    fix initial update of sub device

--- js/blocks.js
+++ js/blocks.js
@@ -410,6 +410,10 @@ function addDeviceUpdateHandler(selector, idx) {
         //        console.log('Already subscribed for '+idx)
         //currently the deviceUpdateHandler handles updates for all blocks that use device <idx>
         // so we have to subscribe only once
+        //but still we have to update the first time.
+        var device = Domoticz.getAllDevices()[idx];
+        if(device)
+            deviceUpdateHandler(selector, idx, Domoticz.getAllDevices()[idx]);
     } else {
         Domoticz.subscribe(idx, true, function (device) {
             deviceUpdateHandler(selector, idx, device)
