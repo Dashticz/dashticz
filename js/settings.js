5d44e1f525f17ffb3ca3bc39f601eaac10a63fec
HansieNL <HansieNL@users.noreply.github.com>
2020-04-18 01:25:18 +0200

    Added security_panel_lock

--- js/settings.js
+++ js/settings.js
@@ -179,6 +179,11 @@ settingList['screen']['security_button_icons'] = {};
 settingList['screen']['security_button_icons']['title'] = language.settings.screen.security_button_icons;
 settingList['screen']['security_button_icons']['type'] = 'checkbox';
 
+settingList['screen']['security_panel_lock'] = {};
+settingList['screen']['security_panel_lock']['title'] = language.settings.screen.security_panel_lock;
+settingList['screen']['security_panel_lock']['type'] = 'checkbox';
+settingList['screen']['security_panel_lock']['help'] = language.settings.screen.security_panel_lock_help;
+
 settingList['screen']['blink_color'] = {};
 settingList['screen']['blink_color']['title'] = language.settings.screen.blink_color;
 settingList['screen']['blink_color']['type'] = 'text';
