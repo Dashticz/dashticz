795492841a408010e2576da839977b9d8dd6e89f
lokonli <sander.lokerse@gmail.com>
2022-11-15 21:01:47 +0100

    Weather: Add KNMI as provider

--- js/loader.js
+++ js/loader.js
@@ -1,6 +1,6 @@
 /*global loadFiles dashtype */
 
-var _DASHTICZ_VERSION=50;
+var _DASHTICZ_VERSION=51;
 var head = document.getElementsByTagName('head')[0],
  script = document.createElement('script');
  
