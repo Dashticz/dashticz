71076a2fd8918745481fae445fe584da2324dd17
lokonli <22852111+lokonli@users.noreply.github.com>
2024-02-12 14:13:53 +0100

    bump loader

--- js/loader.js
+++ js/loader.js
@@ -1,6 +1,6 @@
 /*global loadFiles dashtype */
 
-var _DASHTICZ_VERSION = 111;
+var _DASHTICZ_VERSION = 112;
 var head = document.getElementsByTagName('head')[0],
     script = document.createElement('script');
 
