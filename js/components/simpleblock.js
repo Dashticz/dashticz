6808c1a16d522d25b6552ed14f791e54366c798f
Patrick <81873830+MadPatrick@users.noreply.github.com>
2026-07-26 14:41:40 +0200

    Drop widget/layout editor UI from simple blocks
    Removes widget and visual layout editor controls and related click handlers from the settings block, keeping only the device editor and settings icon. Cleans up associated CSS by deleting large editor-specific style sections, and adds lightweight logo/version styling. The simple block renderers were also aligned with current behavior by removing legacy fixed-height/weather fallback logic and simplifying Spotify/Sonarr call signatures.

--- js/components/simpleblock.js
+++ js/components/simpleblock.js
@@ -1,4 +1,4 @@
-/* global Dashticz DT_function getFullScreenIcon settings loadWeather loadWeatherFull getSpotify DT_button loadSonarr getCoin loadMaps DashticzDeviceEditor DashticzWidgetEditor*/
+/* global Dashticz DT_function getFullScreenIcon settings loadWeather loadWeatherFull getSpotify DT_button loadSonarr getCoin loadMaps DashticzDeviceEditor*/
 //# sourceURL=js/components/simpleblock.js
 var DT_simpleblock = (function () {
   var simpleBlocks = {
@@ -21,10 +21,6 @@ var DT_simpleblock = (function () {
       render: renderResponsiveClock,
     },
     weather: {
-      script: 'js/weather.js',
-      render: renderWeather,
-    },
-    wunderground: {
       script: 'js/weather.js',
       render: renderWeather,
     },
@@ -116,11 +112,16 @@ var DT_simpleblock = (function () {
   }
 
   function renderLogo(me) {
+    var versionHtml =
+      typeof dashticz_version !== 'undefined' && dashticz_version
+        ? '<span class="logo-version">v' + dashticz_version + '</span>'
+        : '';
     return (
       '<div data-id="logo" class="logo col-xs-' +
       me.block.width +
       '">' +
       settings['app_title'] +
+      versionHtml +
       '</div>'
     );
   }
@@ -139,24 +140,14 @@ var DT_simpleblock = (function () {
         case 'settings':
           content +=
             '<span class="settings deviceeditoricon" data-id="deviceeditor" ' +
-            'role="button" aria-label="Open device editor" title="Devices toevoegen">' +
-            '<i class="fas fa-plus" aria-hidden="true"></i></span>';
-          content +=
-            '<span class="settings widgeteditoricon" data-id="widgeteditor" ' +
-            'role="button" aria-label="Open widget editor" title="Widgets toevoegen">' +
-            '<i class="fas fa-puzzle-piece" aria-hidden="true"></i></span>';
-          content +=
-            '<span class="settings layouteditoricon" data-id="layouteditor" ' +
-            'role="button" aria-label="Open visual layout editor" title="Tegels verplaatsen en schalen">' +
-            '<i class="fas fa-arrows-alt" aria-hidden="true"></i></span>';
+            'role="button" aria-label="Open device editor">' +
+            '<i class="fas fa-pencil-alt" aria-hidden="true"></i></span>';
           content +=
             '<span class="settings settingsicon" data-id="settings" ' +
             'data-bs-target="#settingspopup" data-bs-toggle="modal" ' +
-            'role="button" aria-label="Open settings" title="Instellingen">' +
+            'role="button" aria-label="Open settings">' +
             '<i class="fas fa-cog" aria-hidden="true"></i></span>';
           _registerDeviceEditorClick();
-          _registerWidgetEditorClick();
-          _registerLayoutEditorClick();
           break;
 
         case 'fullscreen':
@@ -176,41 +167,11 @@ var DT_simpleblock = (function () {
     });
   }
 
-  function _registerLayoutEditorClick() {
-    $(document)
-      .off('click.layouteditoricon')
-      .on('click.layouteditoricon', '.layouteditoricon', function () {
-        DT_function.loadDTScript('js/layouteditor.js').then(function () {
-          DashticzLayoutEditor.open();
-        });
-      });
-  }
-
-  function _registerWidgetEditorClick() {
-    $(document)
-      .off('click.widgeteditor')
-      .on('click.widgeteditor', '.widgeteditoricon', function () {
-        DT_function.loadDTScript('js/widgeteditor.js').then(function () {
-          DashticzWidgetEditor.open();
-        });
-      });
-  }
-
   function renderMiniclock(me) {
-    var fixedHeight = parseInt(me.block.height, 10);
-    var heightClass = fixedHeight > 0 ? ' fixedheight' : '';
-    var heightStyle =
-      fixedHeight > 0
-        ? ' style="height:' + fixedHeight + 'px !important"'
-        : '';
     return (
-      '<div data-id="miniclock" class="miniclock mh dt_block col-xs-' +
+      '<div data-id="miniclock" class="miniclock col-xs-' +
       me.block.width +
-      ' text-center' +
-      heightClass +
-      '"' +
-      heightStyle +
-      '>' +
+      ' text-center">' +
       '<span class="weekday"></span> <span class="date"></span> <span>&nbsp;&nbsp;&nbsp;&nbsp;</span> <span class="clock"></span>' +
       '</div>'
     );
@@ -253,30 +214,13 @@ var DT_simpleblock = (function () {
 
   function renderWeather(me) {
     function doRender() {
-      var fixedHeight = parseInt(me.block.height, 10);
-      var heightClass = fixedHeight > 0 ? ' fixedheight' : '';
-      var heightStyle =
-        fixedHeight > 0
-          ? ' style="height:' + fixedHeight + 'px !important"'
-          : '';
       me.$mountPoint.html(
-        '<div data-id="weather" class="mh transbg dt_block block_' +
+        '<div data-id="weather" class="block_' +
           me.block.type +
-          ' col-xs-' +
-          me.block.width +
-          ' containsweatherfull' +
-          heightClass +
-          '"' +
-          heightStyle +
-          '></div>'
+          ' containsweatherfull"></div>'
       );
-      if (settings['wu_api'] && settings['wu_city']) {
+      if (settings['wu_api'] !== '' && settings['wu_city'] !== '')
         loadWeatherFull(settings['wu_city'], settings['wu_country']);
-      } else {
-        me.$mountPoint
-          .find('.containsweatherfull')
-          .html('<div class="dt_state">Weather Underground-instellingen ontbreken.</div>');
-      }
     }
     if (typeof loadWeatherFull !== 'function') {
       loadWeatherScript(doRender);
@@ -335,7 +279,7 @@ var DT_simpleblock = (function () {
   
   function renderSpotify(me) {
     me.$mountPoint.html('');
-    getSpotify(me.mountPoint, me.block);
+    getSpotify(me.mountPoint);
   }
 
   function renderTrafficMap(me) {
@@ -380,8 +324,8 @@ var DT_simpleblock = (function () {
     return html;
   }
 
-  function renderSonar(me) {
-    return loadSonarr(me);
+  function renderSonar() {
+    return loadSonarr();
   }
 
   function renderFullScreen(me) {
